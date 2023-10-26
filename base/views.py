d27b37be40cbc24c4abf9ee76127c75e38d52782
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-10-26 11:31:33 +0800

    Applied template inheritance

--- base/views.py
+++ base/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 # Views will also relay our request to any database or templates
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Design with me!'},
+    {'id': 3, 'name': 'Frontend developers!'},
+]
+
 
 def home(request):
-  return render(request, "home.html")
+    return render(request, "base/home.html", {'rooms': rooms})
 
 
 def room(request):
-  return render(request, "room.html")
+    return render(request, "base/room.html")
