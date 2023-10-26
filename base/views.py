1731d5cd4d7b05304e17e2010a741858eb7aab60
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-10-26 09:57:37 +0800

    Create views and urls

--- base/views.py
+++ base/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Views may be functions or classes
 # Views will also relay our request to any database or templates
 # Create your views here.
+
+
+def home(request):
+  return HttpResponse("hello world")
+
+
+def room(request):
+  return HttpResponse("ROOM")
